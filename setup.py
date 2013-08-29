ab062b8273b89aee2714145c2b6d88872758f451
Waldemar <waldemar.schwan@adcloud.com>
2013-08-29 23:13:22 +0200

    added nose and pinoccio to dependecies

--- setup.py
+++ setup.py
@@ -3,7 +3,8 @@ setup(
     name = "pyjabot",
     version = "0.0.0",
     packages = find_packages(),
-    install_requires = ['jabberbot>=0.15', 'xmpppy>=0.5.0rc1'],
+    install_requires = ['jabberbot>=0.15', 'xmpppy>=0.5.0rc1',
+      'nose>=1.3.0', 'pinocchio>=0.3.1'],
     license = "MIT",
     keywords = "jabber bot",
     url = "https://github.com/Velrok/pyjabot"
