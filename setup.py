81873b15be05304a72500986b101b10d3ce9b2ac
Waldemar <waldemar.schwan@adcloud.com>
2013-08-29 12:11:43 +0200

    a first setup.py (untested)

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+setup(
+    name = "pyjabot",
+    version = "0.0.0",
+    packages = find_packages(),
+    install_requires = ['docutils>=2.5', 'jabberbot>=0.15'],
+    license = "MIT",
+    keywords = "jabber bot",
+    url = "https://github.com/Velrok/pyjabot"
+)
