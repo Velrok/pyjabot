b8ac6166b65f42421672f8b36e250527c12f3f3a
Waldemar <waldemar.schwan@adcloud.com>
2013-08-29 13:50:50 +0200

    commented out bot suff; implemented config file loading

--- main.py
+++ main.py
@@ -1,31 +1,40 @@
-from jabberbot import JabberBot, botcmd
+# from jabberbot import JabberBot, botcmd
 import datetime
+import os
+import json
 
-class SystemInfoJabberBot(JabberBot):
-    @botcmd
-    def serverinfo( self, mess, args):
-        """Displays information about the server"""
-        version = open('/proc/version').read().strip()
-        loadavg = open('/proc/loadavg').read().strip()
+def get_config_path():
+  home = os.path.abspath(os.environ["HOME"])
+  return os.path.join(home, ".pyjabot-config.json")
 
-        return '%s\n\n%s' % ( version, loadavg, )
+def get_config():
+  return json.load(open(get_config_path()))
 
-    @botcmd
-    def time( self, mess, args):
-        """Displays current server time"""
-        return str(datetime.datetime.now())
-
-    @botcmd
-    def rot13( self, mess, args):
-        """Returns passed arguments rot13'ed"""
-        return args.encode('rot13')
-
-    @botcmd
-    def whoami(self, mess, args):
-        """Tells you your username"""
-        return mess.getFrom().getStripped()
-
-username = 'my-jabberid@jabberserver.example.org'
-password = 'my-password'
-bot = SystemInfoJabberBot(username,password)
-bot.serve_forever()
+#class SystemInfoJabberBot(JabberBot):
+#    @botcmd
+#    def serverinfo( self, mess, args):
+#        """Displays information about the server"""
+#        version = open('/proc/version').read().strip()
+#        loadavg = open('/proc/loadavg').read().strip()
+#
+#        return '%s\n\n%s' % ( version, loadavg, )
+#
+#    @botcmd
+#    def time( self, mess, args):
+#        """Displays current server time"""
+#        return str(datetime.datetime.now())
+#
+#    @botcmd
+#    def rot13( self, mess, args):
+#        """Returns passed arguments rot13'ed"""
+#        return args.encode('rot13')
+#
+#    @botcmd
+#    def whoami(self, mess, args):
+#        """Tells you your username"""
+#        return mess.getFrom().getStripped()
+#
+#username = 'my-jabberid@jabberserver.example.org'
+#password = 'my-password'
+#bot = SystemInfoJabberBot(username,password)
+#bot.serve_forever()
