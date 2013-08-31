1031573153950cce0c17f0600309bffc614ecb51
Waldemar <waldemar.schwan@adcloud.com>
2013-08-31 16:22:47 +0200

    added doc opt dependecy

--- setup.py
+++ setup.py
@@ -4,7 +4,8 @@ setup(
     version = "0.0.0",
     packages = find_packages(),
     install_requires = ['jabberbot>=0.15', 'xmpppy>=0.5.0rc1',
-      'nose>=1.3.0', 'pinocchio>=0.3.1', "sniffer>=0.3.2"],
+      'nose>=1.3.0', 'pinocchio>=0.3.1', "sniffer>=0.3.2",
+      "docopt>=0.6.1"],
     license = "MIT",
     keywords = "jabber bot",
     url = "https://github.com/Velrok/pyjabot"
