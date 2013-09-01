38de0538be3abde16fbc877fd4c683814caa9ea3
Nesurion <maik.schwan@gmail.com>
2013-09-01 11:41:10 +0200

    Implementing find function in progress

--- main.py
+++ main.py
@@ -1,7 +1,37 @@
 from jabberbot import JabberBot, botcmd
+from docopt import docopt, DocoptExit
 import datetime
 import os
 import json
+import re
+
+def waldemars_fn(path):
+  return [
+  {'filepath': "True Blood/Season 06/True Blood 6x10.mkv",
+  'showname': "True Blood",
+  'episode#': 10,
+  'season#': 6,
+  'ext': "mkv"},
+  {'filepath': "True Blood/Season 06/True Blood 6x11.mkv",
+  'showname': "True Blood",
+  'episode#': 11,
+  'season#': 6,
+  'ext': "mkv"},
+  {'filepath': "Warehouse 13/Season 02/Warehouse 13 2x5.mkv",
+  'showname': "Warehouse 13",
+  'episode#': 5,
+  'season#': 2,
+  'ext': "mkv"},
+  ]
+
+def maiks_filter(episodes, pattern, season=None, episode=None):
+  filtered_episode_list = []
+  pattern_as_regex = re.compile(pattern)
+  def f(x):
+    return pattern_as_regex.match(x["showname"])
+  return filter(f, episodes) 
+
+
 
 def get_config_path():
   home = os.path.abspath(os.environ["HOME"])
@@ -48,7 +78,32 @@ class TvButtler(JabberBot):
 
     else:
       return "Sorry, command not avabile. Type help list for a list of commands."
-    
+
+  @botcmd
+  def find(self, message, args):
+    """
+    Find episondes based on regex.
+
+    usage:
+      find <pattern> <season_number> <episode_number>
+
+    """
+    try:
+      argments  = docopt(self.find.__doc__, args.split(" "))
+    except DocoptExit:
+      return self.find.__doc__
+
+    pattern   = argments['<pattern>']
+    season    = argments['<season_number>']
+    episode   = argments['<episode_number>']
+
+    conf      = get_config()
+    shows_dir = conf["shows_dir"]
+    episodes  = waldemars_fn(shows_dir)
+
+    found_episodes = maiks_filter(episodes, pattern, season, episode) 
+    nice_string = make_nice_string(found_episodes)
+    return nice_string
 
 
 
--- test_main.py
+++ test_main.py
@@ -1,7 +1,60 @@
-from nose.tools import nottest
+from main import *
+from nose.tools import nottest, eq_
+
+all_episodes = [
+  {'filepath': "True Blood/Season 06/True Blood 6x10.mkv",
+  'showname': "True Blood",
+  'episode#': 10,
+  'season#': 6,
+  'ext': "mkv"},
+  {'filepath': "True Blood/Season 06/True Blood 6x11.mkv",
+  'showname': "True Blood",
+  'episode#': 11,
+  'season#': 6,
+  'ext': "mkv"},
+  {'filepath': "Warehouse 13/Season 02/Warehouse 13 2x5.mkv",
+  'showname': "Warehouse 13",
+  'episode#': 5,
+  'season#': 2,
+  'ext': "mkv"},
+  ]
+
+
 def test_true():
   assert True
 
 @nottest
 def test_fail():
   assert False
+
+def test_maiks_filter_returns_2_True_Blood_Entrys():
+  result = maiks_filter(all_episodes, "True Blood")
+  expectation = [{'filepath': "True Blood/Season 06/True Blood 6x10.mkv",
+  'showname': "True Blood",
+  'episode#': 10,
+  'season#': 6,
+  'ext': "mkv"},
+  {'filepath': "True Blood/Season 06/True Blood 6x11.mkv",
+  'showname': "True Blood",
+  'episode#': 11,
+  'season#': 6,
+  'ext': "mkv"}]
+  eq_(result, expectation)  
+
+def test_maiks_filter_returns_1_Warehouse_Entry():
+  result = maiks_filter(all_episodes, "Warehouse")
+  expectation = [{'filepath': "Warehouse 13/Season 02/Warehouse 13 2x5.mkv",
+  'showname': "Warehouse 13",
+  'episode#': 5,
+  'season#': 2,
+  'ext': "mkv"}]
+  eq_(result, expectation)
+
+def test_maiks_filter_returns_TrueBlood_EP10():
+  result = maiks_filter(all_episodes, "True Blood 6 10")
+  expectation = [{'filepath': "True Blood/Season 06/True Blood 6x10.mkv",
+  'showname': "True Blood",
+  'episode#': 10,
+  'season#': 6,
+  'ext': "mkv"}]
+  eq_(result, expectation)
